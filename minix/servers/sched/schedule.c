da70f0301bc3bbe71b3c6edbea5f06a6a11d3f1e
sidistic <siddharth.538@gmail.com>
2019-02-14 04:31:09 +0530

    Scheduler is now close to FIFO

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -102,7 +102,7 @@ int do_noquantum(message *m_ptr)
 
 	rmp = &schedproc[proc_nr_n];
 	if (rmp->priority < MIN_USER_Q) {
-		rmp->priority += 1; /* lower priority */
+		// rmp->priority += 1; /* lower priority */
 	}
 
 	if ((rv = schedule_process_local(rmp)) != OK) {
@@ -362,7 +362,7 @@ static void balance_queues(minix_timer_t *tp)
 	for (proc_nr=0, rmp=schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
 		if (rmp->flags & IN_USE) {
 			if (rmp->priority > rmp->max_priority) {
-				rmp->priority -= 1; /* increase priority */
+				// rmp->priority -= 1; /* increase priority */
 				schedule_process_local(rmp);
 			}
 		}
